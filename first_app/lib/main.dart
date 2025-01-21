d091c6bec20ccec8f7ed15f78e894c8a0e1bcc0f
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2025-01-22 00:41:58 +0600

    21

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Text('Hello Gys!'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(31, 87, 0, 67),
+                Color.fromARGB(66, 180, 28, 28)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello Gys!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 48,
+              ),
+            ),
+          ),
         ),
       ),
     ),
