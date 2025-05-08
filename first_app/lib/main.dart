3d7484facdc931e1a8bd6f49bdf03b8e53837b79
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2025-05-08 17:13:33 +0600

    1

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/gradient_container.dart';
+// import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: GradientContainer( ),
+        // backgroundColor: Color.fromARGB(255, 200, 238, 255),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 148, 171, 240),
+                const Color.fromARGB(26, 237, 114, 114)
+              ],
+              begin: Alignment.bottomRight,
+              end: Alignment.center,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello ,Shoumik !',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        ),
       ),
     ),
   );
 }
-
